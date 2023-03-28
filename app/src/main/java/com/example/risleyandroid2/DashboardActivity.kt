b4c50924addda56e6143f188ae607884a6f8b935
luthor628 <abdulrahmanabdulaziz628@gmail.com>
2023-03-28 12:39:47 +0300

    first commit

---
+++ app/src/main/java/com/example/risleyandroid2/DashboardActivity.kt
@@ -0,0 +1,11 @@
+package com.example.risleyandroid2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
